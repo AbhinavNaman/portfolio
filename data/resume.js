6c06826325afda4cff3cb5a96c42d8272edb053a
AbhinavNaman <abhinavnaman3@gmail.com>
2025-07-18 12:30:54 +0530

    updated resume

--- data/resume.js
+++ data/resume.js
@@ -1,4 +1,6 @@
-export const resume_view = "https://drive.google.com/file/d/1dvgbiI9dBi1u9N3VQaQZt5QLk4TRmK9O/view"
-export const resume_preview = "https://drive.google.com/file/d/1dvgbiI9dBi1u9N3VQaQZt5QLk4TRmK9O/preview"
+export const resume_view = "https://drive.google.com/file/d/1qxLNEhGAH8q8awyT3LCJQscYhiTEndLt/view"
+export const resume_preview = "https://drive.google.com/file/d/1qxLNEhGAH8q8awyT3LCJQscYhiTEndLt/preview"
+// export const resume_view = "https://drive.google.com/file/d/1dvgbiI9dBi1u9N3VQaQZt5QLk4TRmK9O/view"
+// export const resume_preview = "https://drive.google.com/file/d/1dvgbiI9dBi1u9N3VQaQZt5QLk4TRmK9O/preview"
 
 //https://drive.google.com/file/d/<-----file_id------->/preview
